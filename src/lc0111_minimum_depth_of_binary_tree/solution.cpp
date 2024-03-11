6aaee1ea1e55d1603ce68a60ec571024666c1f87
Dino Source <dinosource.cpp@gmail.com>
2024-03-10 23:09:47 -0700

    Provide fix for lc111 implementation

--- src/lc0111_minimum_depth_of_binary_tree/solution.cpp
+++ src/lc0111_minimum_depth_of_binary_tree/solution.cpp
@@ -11,12 +11,11 @@ int Solution::minDepth(TreeNode* root) {
     std::list<TreeNode*> queue;
     queue.push_back(root);
 
-    while (!queue.empty()) {
+    for (/*intentionally empty section*/; !queue.empty(); ++depth) {
         auto nodesCount {queue.size()};
-        TreeNode* node;
-        for (size_t i = 0; i < nodesCount; ++i, ++depth)
+        for (size_t i = 0; i < nodesCount; ++i)
         {
-            node = queue.front();
+            TreeNode* node = queue.front();
             queue.pop_front();
             if (node->left == nullptr && node->right == nullptr) {
                 return depth;
@@ -30,7 +29,6 @@ int Solution::minDepth(TreeNode* root) {
                 queue.push_back(node->right);
             }
         }
-        
     }
 
     return depth;
